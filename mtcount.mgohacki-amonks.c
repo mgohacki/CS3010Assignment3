135968efe7c2e31f663409f8ced1c0f6c06e0c9b
mirogohacki <mgohacki@uvm.edu>
2023-10-04 12:16:33 -0400

    Adding stuff from instructions

--- mtcount.mgohacki-amonks.c
+++ mtcount.mgohacki-amonks.c
@@ -1,3 +1,18 @@
 //
 // Created by Miro Gohacki on 10/4/23.
 //
+
+#include <stdio.h>
+
+int prand() {
+    static int p = 1;
+    const int a = 105491;
+    const int b = 733;
+    const int c = 6619;
+    const int base = 100;
+    int val;
+    p = p*a + b;
+    p = p % c;
+    val = p % base;
+    return val;
+}
